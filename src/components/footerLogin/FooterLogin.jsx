71c777f699461f4cc9afb83e1fcfbb441af7b89f
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-09-07 22:13:37 -0500

    fix: add the correct links

--- src/components/footerLogin/FooterLogin.jsx
+++ src/components/footerLogin/FooterLogin.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom"
 
 // eslint-disable-next-line react/prop-types
 function FooterLogin({text, wordlink, path}) {
   return (
     <p className="mt-6 text-sm text-center text-gray-400">
    {text}
-    <a href={path} className="text-primary hover:underline pl-2">
+    <Link to={path} className="text-primary hover:underline pl-2">
       {wordlink}
-    </a>
+    </Link>
   </p>
   )
 }
--- src/layout/LoginLayout.jsx
+++ src/layout/LoginLayout.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import DrawerImage from "../components/drawerImage/DrawerImage";
 
 // eslint-disable-next-line react/prop-types
@@ -9,11 +10,13 @@ function LoginLayout({ children }) {
         <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
           <div className="flex-1">
             <div className="text-center">
-              <div className="flex justify-center mx-auto fill-primary h-8">
-                <svg viewBox="0 0 20 9" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M17 9H20L10 0L0 9H3L10 2.69L17 9ZM5 1.81V0H2V4.5L5 1.81Z" />
-                </svg>
-              </div>
+              <Link to="/">
+                <div className="flex justify-center mx-auto fill-primary h-8">
+                  <svg viewBox="0 0 20 9" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17 9H20L10 0L0 9H3L10 2.69L17 9ZM5 1.81V0H2V4.5L5 1.81Z" />
+                  </svg>
+                </div>
+              </Link>
               <p className="mt-3 text-gray-500 ">
                 Inicia sesión para acceder a tu cuenta
               </p>
