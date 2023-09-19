237848052d2d60d014f587ca86e8d87c740b010c
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-09-19 15:23:17 -0500

    fix: remove unnecessary link from footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,11 +6,8 @@ function Footer() {
     <footer className="bg-primary rounded-lg shadow m-4 mt-20 text-white">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-smsm:text-center ">
-          © {new Date().getFullYear()}{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Ganaderos Del Caribe
-          </a>
-          . All Rights Reserved.
+          © {new Date().getFullYear()} Ganaderos Del Caribe . All Rights
+          Reserved.
         </span>
         <FooterItemsLogos socialNetworks={socialNetworksFooter} />
       </div>
