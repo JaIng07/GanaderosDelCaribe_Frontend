43ff45f77601310bdbbe1530c67e8df8383fcdae
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-09-26 01:37:43 -0500

    fix: change the icon to open the sidebar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import DropdownUser from "../DropdownHeader/DropdownUser";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 // eslint-disable-next-line react/prop-types
 const Header = ({ sidebarOpen, setSidebarOpen }) => {
@@ -6,17 +7,16 @@ const Header = ({ sidebarOpen, setSidebarOpen }) => {
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-md">
       <div className="flex flex-grow items-center justify-between py-4 px-4 shadow-2 md:px-6 2xl:px-11">
         <div className="flex items-center gap-2 sm:gap-4 ">
-          {/* <!-- Hamburger Toggle BTN --> */}
-          <button
+          <Bars3Icon
+            color="black"
+            height={35}
             aria-controls="sidebar"
             onClick={(e) => {
               e.stopPropagation();
               setSidebarOpen(!sidebarOpen);
             }}
             className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm lg:hidden"
-          >
-            Open
-          </button>
+          />
         </div>
 
         <div className="flex items-center gap-3 2xsm:gap-7">
