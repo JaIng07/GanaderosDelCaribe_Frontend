58d40cf6ff58517e31ac7a442e3f626c117a6136
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-10-03 17:30:07 -0500

    feat: muestra un animal en su propia ruta

--- src/components/animalCard/AnimalCardItems.jsx
+++ src/components/animalCard/AnimalCardItems.jsx
@@ -1,45 +1,52 @@
+import { ChevronRightIcon } from '@heroicons/react/20/solid';
+import { Link } from 'react-router-dom';
+
 // eslint-disable-next-line react/prop-types
 function AnimalCardItems({ id, raza, edad, peso, imagenURL }) {
   const image = imagenURL
     ? imagenURL
-    : "https://static.vecteezy.com/system/resources/thumbnails/004/141/669/small/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg";
+    : "https://i.pinimg.com/474x/18/cc/58/18cc58f204186fe709ebcb229895b43e.jpg";
 
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 font-montserrat font-normal">
-      <h2 className="text-xl mb-4">Información del Animal</h2>
-      <div className="grid justify-items-center">
+    <div className="bg-white rounded-lg shadow-md p-4 font-montserrat font-normal max-h-[1000px]">
+      <div className="grid justify-items-center w-full">
         <img
           src={image}
           alt="Foto del animal"
-          className="rounded-md mb-12 center"
+          className="w-full h-48 object-cover rounded-md mb-5 mx-auto shadow-lg"
         />
       </div>
       <div className="grid grid-cols-2 gap-4">
         <div className="col-span-2 md:col-span-1">
-          <div className="bg-gray-100 p-3 rounded-lg">
+          <div className="bg-gray-50 p-3 rounded-lg">
             <p className="text-gray-600 text-sm">Número de Identificación:</p>
             <p className="text-lg font-medium pt-1">{id}</p>
           </div>
         </div>
         <div className="col-span-2 md:col-span-1">
-          <div className="bg-gray-100 p-3 rounded-lg">
+          <div className="bg-gray-50 p-3 rounded-lg">
             <p className="text-gray-600 text-sm">Raza:</p>
             <p className="text-lg font-medium pt-1">{raza}</p>
           </div>
         </div>
         <div className="col-span-2 md:col-span-1">
-          <div className="bg-gray-100 p-3 rounded-lg">
+          <div className="bg-gray-50 p-3 rounded-lg">
             <p className="text-gray-600 text-sm">Edad:</p>
             <p className="text-lg font-medium pt-1">{edad}</p>
           </div>
         </div>
         <div className="col-span-2 md:col-span-1">
-          <div className="bg-gray-100 p-3 rounded-lg">
+          <div className="bg-gray-50 p-3 rounded-lg">
             <p className="text-gray-600 text-sm">Peso:</p>
             <p className="text-lg font-medium pt-1">{peso}</p>
           </div>
         </div>
       </div>
+      <hr className="my-4" />
+      <Link to={`${id}`} className="bg-transparent text-black px-4 hover:text-primary hover:underline transition duration-300 ease-in-out flex items-center ">
+        Ver más
+        <ChevronRightIcon className="w-5 h-5 ml-1" />
+      </Link>
     </div>
   );
 }
--- src/pages/dashboard/EmployeeRegistration.jsx
+++
@@ -1,7 +0,0 @@
-import DashboardLayout from "../../layout/DashboardLayout";
-
-const EmployeeRegistration = () => {
-  return <DashboardLayout>Registro de trabajadores</DashboardLayout>;
-};
-
-export default EmployeeRegistration;
