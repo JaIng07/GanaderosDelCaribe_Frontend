8dbb3bcaf61a609ac3aade0c26a8416274c80eb3
“JuanDiaz” <juanluisdiazguerrero@gmail.com>
2023-11-15 23:49:44 -0500

    cierra sesion con token expirado

--- src/pages/dashboard/ActivityRegister:/ActivityRegister.jsx
+++ src/pages/dashboard/ActivityRegister:/ActivityRegister.jsx
@@ -2,53 +2,10 @@ import { useState, useEffect } from "react";
 import { DragDropContext } from "react-beautiful-dnd";
 import Column from "../../../components/DragAndDrop/Column";
 import DashboardLayout from "../../../layout/DashboardLayout";
-import { changeColumnActivity, getActivities } from "../../../services/activity.services";
+import { getActivities } from "../../../services/activity.services";
 import { decodedToken, getToken } from "../../../helpers/JWT";
+import { onDragEnd } from "../../../helpers/dragAndDrop";
 
-const onDragEnd = async(result, columns, setColumns, setReloadData ) => {
-
-  if (!result.destination) return;
-
-  const { source, destination, draggableId} = result;
-
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
-    const [removed] = sourceItems.splice(source.index, 1);
-    destItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems,
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems,
-      },
-    });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems,
-      },
-    });
-  }
-
-  if(destination.droppableId !== source.droppableId){
-    await changeColumnActivity(draggableId, destination.droppableId)
-    setReloadData(prev=>!prev)
-  }
-
-};
 
 const ActivityRegister = ({ idUser, children, reloadDataAdmin }) => {
   const [columns, setColumns] = useState([]);
